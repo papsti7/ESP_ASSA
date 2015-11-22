8587030cb5b627735ca841e4f432e0582696ed6c
Stefan Papst <stefan.papst@gmx.net>
2015-11-22 17:56:52 +0100

    -implemented CodeCorrectness Function
    -still an issue with the frees()

--- src/assa.c
+++ src/assa.c
@@ -14,6 +14,7 @@
 // Latest Changes: 21.11.2015 (by Stefan Papst)
 //------------------------------------------------------------------------------
 //
+/*this is a special flag in visual studio for secure function errors/ Can be commented if there is a error in gcc*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -29,16 +30,17 @@
 #define SUCCESS 0
 #define FALSE_ARGUMENTS 1
 #define OUT_OF_MEMORY 2
-#define COULD_NOT_PARSE_FILE 3
+#define PARSE_FILE_ERROR 3
 #define READING_FILE_FAIL 4
 
 #define TRUE 0
 #define FALSE 1
+#define NEUTRAL -1
 
 
-
+/*------------HEADER NEED TO BE DONE-----------*/
 int readCodeFromFile(char** data_segment, char* name);
-int parseCode(char* data_segment);
+int checkCodeCorrectness(char* data_segment);
 int checkCommandOrComment(char current_char);
 
 
@@ -83,10 +85,20 @@ int main(int argc, char* argv[])
     printf("[ERR] usage: ./assa [-e brainfuck_filnename]\n");
     return FALSE_ARGUMENTS;
   }
+  if (checkCodeCorrectness(data_segment) != SUCCESS)
+  {
+    printf("[ERR] parsing of input failed\n");
+    //---------This free is failing with visual studio, I'll test it on unix and fix it later. --------//
+    //free(data_segment);
+    return PARSE_FILE_ERROR;
+  }
   
   puts(data_segment);
+  
   getchar();
-  free(data_segment);
+  
+  //---------This free is failing with visual studio, I'll test it on unix and fix it later. --------//
+  //free(data_segment);
   return return_value;
 }
 
@@ -99,7 +111,7 @@ int readCodeFromFile(char** data_segment, char* name)
     return OUT_OF_MEMORY;
   }
   //set end of string
-  (*data_segment)[1023] = '\0';
+  (*data_segment)[(int)(1024 * 0.8 + 1)] = '\0';
 
   //open file
   FILE* file;
@@ -114,7 +126,7 @@ int readCodeFromFile(char** data_segment, char* name)
   
   int current_char;
   int data_segment_size = 1024;
-  int counter = 0;
+  int counter = 1;
   while ((current_char = getc(file)) != EOF)
   {
     if (checkCommandOrComment(current_char) == TRUE)
@@ -142,7 +154,7 @@ int readCodeFromFile(char** data_segment, char* name)
         (*data_segment)[counter] = current_char;
         (*data_segment)[counter + 1] = '\0';
       }
-      counter++;
+      (*data_segment)[0] = (++counter) + 1;
     }
     
   }
@@ -150,6 +162,24 @@ int readCodeFromFile(char** data_segment, char* name)
 
   fclose(file);
   return SUCCESS;
+}
+
+int checkCodeCorrectness(char* data_segment)
+{
+  //1 because in data_segment[0] is a value of how long the code part is. 
+  int counter = 1;
+  int open_brackets_count = 0;
+  int close_brackets_count = 0;
+  while (data_segment[counter] != '\0')
+  {
+    if(data_segment[counter] == '[')
+      open_brackets_count++;
+    else if(data_segment[counter] == ']')
+      close_brackets_count++;
+    counter++;
+  }
+
+  return open_brackets_count - close_brackets_count;
 }
 
 int checkCommandOrComment(char current_char)
@@ -184,4 +214,4 @@ int checkCommandOrComment(char current_char)
       return FALSE;
       break;
   }
-}
+}                                                                                                                                   
